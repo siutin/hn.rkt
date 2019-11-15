d4e874685f40411bd9029ce70135c7b6b16ad1c5
siutin <osiutino@gmail.com>
2019-11-15 22:27:59 +0800

    reuse connection

--- main.rkt
+++ main.rkt
@@ -3,34 +3,32 @@
 (require net/http-client)
 (require json)
 
-(define (hw/base/api-get url)
-  (define-values (status headers in)
-    (let ([hc (http-conn)])
-      (http-conn-open!
-            hc
-            "hacker-news.firebaseio.com"
-            #:ssl? #t
-            #:port 443
-            #:auto-reconnect? #t)
-      (http-conn-sendrecv!
-        hc      
-        (format "https://hacker-news.firebaseio.com/~a" url)
-        #:method "GET")))
-  (let ([s (string->jsexpr (port->string in))])
-    (close-input-port in)
-    s))
+(let ([hc (http-conn)])
+  (http-conn-open!
+    hc
+    "hacker-news.firebaseio.com"
+    #:ssl? #t
+    #:port 443
+    #:auto-reconnect? #t)
 
-(define (hw/api/topstories)
-  (hw/base/api-get "/v0/topstories.json"))
+  (define (hw/base/api-get url)
+    (define-values (status headers in)
+        (http-conn-sendrecv!
+          hc
+          (format "https://hacker-news.firebaseio.com/~a" url)
+          #:method "GET"))
+    (let ([s (string->jsexpr (port->string in))])
+      (close-input-port in)
+      s))
 
-(define (hw/api/item id)
-  (hw/base/api-get (format "/v0/item/~a.json" id)))
+  (define (hw/api/topstories)
+    (hw/base/api-get "/v0/topstories.json"))
 
-(define (hw/index)
-  (for ([item-id (in-list (hw/api/topstories))])
-    (displayln (hw/api/item item-id))))
+  (define (hw/api/item id)
+    (hw/base/api-get (format "/v0/item/~a.json" id)))
 
-(hw/index)
+  (define (hw/index)
+    (for ([item-id (in-list (take (hw/api/topstories) 5))])
+      (displayln (hw/api/item item-id))))
 
-;;; (hw/topstories)
-;;; (hw/item 21534133)
+  (hw/index))
