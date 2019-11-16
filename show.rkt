6e7caf006488408aa973cc990eae2dc11ca4efa2
siutin <osiutino@gmail.com>
2019-11-16 19:37:07 +0800

    review show

--- show.rkt
+++ show.rkt
@@ -12,18 +12,25 @@
                    [border 2]))
 (send frame show #t)
 
-(define (add-frame-message-box message author parent-object)
-  (let* ([p (new panel%
-                    [alignment (list 'left 'top)]
-                    (parent parent-object)
-                    (style (list 'border)))])
+(define (add-frame-message-box model parent-object)
+  (let ([panel (new vertical-pane%
+                  (parent parent-object)
+                  [alignment (list 'left 'top)])])
     (new message%
-     (parent p)
-     (label message))
+     (parent panel)
+     [auto-resize #t]
+     [font small-control-font]
+     (label (format "@~a" (hash-ref model 'author ""))))
     (new message%
-     (parent p)
-     (label author))
-    p))
+     (parent panel)
+     [auto-resize #t]
+     (label (hash-ref model 'title "")))
+    (new message%
+     (parent panel)
+     [auto-resize #t]
+     [font small-control-font]
+     (label (hash-ref model 'url "")))
+    panel))
 
 (define hc (http-conn))
 (http-conn-open!
@@ -56,11 +63,19 @@
   (date-display-format 'iso-8601)
   (let* ([f (hw/show 21534283)]
          [item (touch f)]
+         [h (make-hasheq)]
          [title (hash-ref item 'title "")]
          [by (hash-ref item 'by "")]
          [time (hash-ref item 'time "")]
-         [dt-str (date->string (seconds->date time))]
+         [posted-at (date->string (seconds->date time))]
          [score (hash-ref item 'score 0)]
-         [descendants (hash-ref item 'descendants 0)])
-      (add-frame-message-box title by frame)
+         [descendants (hash-ref item 'descendants 0)]
+         [url (hash-ref item 'url "")])
+      (hash-set! h 'title title)
+      (hash-set! h 'author by)
+      (hash-set! h 'posted-at posted-at)
+      (hash-set! h 'point score)
+      (hash-set! h 'coment-count descendants)
+      (hash-set! h 'url url)
+      (add-frame-message-box h frame)
       (displayln (format "~a ~a ~a ~a ~a\n" title by time score descendants))))
