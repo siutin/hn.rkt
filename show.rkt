f6bd8d6d0e9b97c1c27f971f04578d90f8047868
siutin <osiutino@gmail.com>
2019-11-17 16:53:14 +0800

    display post content in list box

--- show.rkt
+++ show.rkt
@@ -13,40 +13,20 @@
                    [border 2]))
 (send frame show #t)
 
-(define text (new text%))
-(define canvas (new editor-canvas% [parent frame]
-                                   [editor text]
-                                   [style (list 'no-focus 'no-hscroll)]))
-
-(define style-delta (make-object style-delta%                                 
-                                 'change-normal-color))
+(define frame-comments-box-data empty)
+(define frame-comments-box
+  (new list-box%
+        (label #f)
+        (parent frame)
+        (choices frame-comments-box-data)
+        (style (list 'single))
+        (min-height 600)))
 
 (define (add-frame-comment-box s)
-  (let* ([ss (string-split-by-length s #:size 100)])
+  (let* ([ss (string-split-by-length s #:size 145)])
     (displayln (length ss))
     (for/list ([t ss])
-      (send text change-style style-delta)
-      (send text insert (format "~a\n" t)))))
-
-(define (add-frame-message-box model parent-object)
-  (let ([panel (new vertical-pane%
-                  (parent parent-object)
-                  [alignment (list 'left 'top)])])
-    (new message%
-     (parent panel)
-     [auto-resize #t]
-     [font small-control-font]
-     (label (format "@~a" (hash-ref model 'author ""))))
-    (new message%
-     (parent panel)
-     [auto-resize #t]
-     (label (hash-ref model 'title "")))
-    (new message%
-     (parent panel)
-     [auto-resize #t]
-     [font small-control-font]
-     (label (hash-ref model 'url "")))
-    panel))
+      (send frame-comments-box append t))))
 
 (define hc (http-conn))
 (http-conn-open!
@@ -82,28 +62,35 @@
 
 (begin
   (date-display-format 'iso-8601)
-  (let* ([comments (hw/show 21534283)])
-    (for/list ([comment comments])
-      (let* ([item comment]
-             [h (make-hasheq)]
-             [title (hash-ref item 'title "")]
-             [by (hash-ref item 'by "")]
-             [time (hash-ref item 'time "")]
-             [posted-at (date->string (seconds->date time))]
-             [score (hash-ref item 'score 0)]
-             [descendants (hash-ref item 'descendants 0)]
-             [url (hash-ref item 'url "")]
-             [kids (hash-ref item 'kids '())]
-             [data-text (hash-ref item 'text "")]
-             [data-title-or-text (if (non-empty-string? title)
-                                     title
-                                     data-text)])
-        (send text insert (format "@~a\n" by))
-        (add-frame-comment-box data-title-or-text)
-        (send text insert "----\n")
-        (displayln (format "@~a\n" by))
-        (displayln (format "#~a ~a ~a ~a ~a\n" time score descendants url kids))
-        (for/list ([t (string-split-by-length data-text #:size 100)])
-          (displayln (format ">~a\n" t)))))))
+  (let ([panel (new vertical-panel%
+                (parent frame)
+                [border 0]
+                [spacing 0]
+                [vert-margin 0]
+                [horiz-margin 0]
+                [alignment (list 'left 'top)])])
+    (let* ([comments (hw/show 21534283)])
+      (for/list ([comment comments])
+        (let* ([item comment]
+               [h (make-hasheq)]
+               [title (hash-ref item 'title "")]
+               [by (hash-ref item 'by "")]
+               [time (hash-ref item 'time "")]
+               [posted-at (date->string (seconds->date time))]
+               [score (hash-ref item 'score 0)]
+               [descendants (hash-ref item 'descendants 0)]
+               [url (hash-ref item 'url "")]
+               [kids (hash-ref item 'kids '())]
+               [data-text (hash-ref item 'text "")]
+               [data-title-or-text (if (non-empty-string? title)
+                                       title
+                                       data-text)])
+          (send frame-comments-box append (format "@~a" by))
+          (add-frame-comment-box data-title-or-text)
+          (send frame-comments-box append "-------------")
+          (displayln (format "@~a\n" by))
+          (displayln (format "#~a ~a ~a ~a ~a\n" time score descendants url kids))
+          (for/list ([t (string-split-by-length data-text #:size 100)])
+            (displayln (format ">~a\n" t))))))))
 
       
